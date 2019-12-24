e31e7b05f340999f90216b79fb44cf10b2bff721
smitagaikwad25 <gaikwadsmit021@gmail.com>
2019-12-24 15:16:29 +0530

    Add : function to validate mail id

--- src/main/java/com/bridgelabz/UserRegistration.java
+++ src/main/java/com/bridgelabz/UserRegistration.java
@@ -20,5 +20,13 @@ public class UserRegistration {
             return userLastName;
         return "Invalid";
     }
+
+    public String analyseMailId(String mailId) {
+        Pattern pattern = Pattern.compile("^[a-zA-Z]{1,}([.]{1})?[0-9a-zA-Z]*[@]{1}[a-zA-Z0-9]{1,}[.]{1}[a-zA-Z]{2,4}([.]{1})?([a-zA-Z]{2,3})?$");
+        Matcher matcher = pattern.matcher(mailId);
+        if (matcher.matches())
+            return mailId;
+        return "Invalid";
+    }
 }
 
